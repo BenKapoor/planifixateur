1d17fba5bb2c2f16089467dd66edeb028caa5354
Benjamin <benjaminkapoor@yahoo.fr>
2021-05-08 21:30:41 +0200

    Tache

--- src/main/java/com/planifixateur/model/Tache.java
+++ src/main/java/com/planifixateur/model/Tache.java
@@ -1,50 +1,48 @@
 package com.planifixateur.model;
 
-import com.planifixateur.model.dto.TacheDto;
-
 public class Tache {
 
 //	@Id
 //	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idTache;
-
-	private String libelle;
-
-	public static Tache from(TacheDto tacheDto) {
-		Tache tache = new Tache();
-		tache.setLibelle(tacheDto.getLibelle());
-		return tache;
-	}
-
-	public Tache() {
-		super();
-	}
-
-	public Tache(String libelle) {
-		super();
-		this.libelle = libelle;
-	}
-
-	public Tache(Long id, String libelle) {
-		super();
-		this.idTache = id;
-		this.libelle = libelle;
-	}
-
-	public Long getId() {
-		return idTache;
-	}
-
-	public void setId(Long id) {
-		this.idTache = id;
-	}
-
-	public String getLibelle() {
-		return libelle;
-	}
-
-	public void setLibelle(String libelle) {
-		this.libelle = libelle;
-	}
+//	private Long idTache;
+//
+//	private String libelle;
+//
+//	public static Tache from(TacheDto tacheDto) {
+//		Tache tache = new Tache();
+//		tache.setLibelle(tacheDto.getLibelle());
+//		return tache;
+//	}
+//
+//	public Tache() {
+//		super();
+//	}
+//
+//	public Tache(String libelle) {
+//		super();
+//		this.libelle = libelle;
+//	}
+//
+//	public Tache(Long id, String libelle) {
+//		super();
+//		this.idTache = id;
+//		this.libelle = libelle;
+//	}
+//
+//	public Long getId() {
+//		return idTache;
+//	}
+//
+//	public void setId(Long id) {
+//		this.idTache = id;
+//	}
+//
+//	public String getLibelle() {
+//		return libelle;
+//	}
+//
+//	public void setLibelle(String libelle) {
+//		this.libelle = libelle;
+//	}
 
 }
