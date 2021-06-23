e8bc40fed4e21e301d02e49354cf456309a71f2e
Benjamin <benjaminkapoor@yahoo.fr>
2021-06-23 12:36:11 +0200

    heroku config

--- src/main/java/com/planifixateur/configuration/DatabaseConfig.java
+++ src/main/java/com/planifixateur/configuration/DatabaseConfig.java
@@ -4,11 +4,12 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-//@Configuration
+@Configuration
 public class DatabaseConfig {
 
 	@Value("${spring.datasource.url}")
